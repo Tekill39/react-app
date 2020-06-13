a19af73b4679962b3d38f5c2edf0d9e1d0863b7e
Tekill39 <minit009@yandex.ru>
2020-06-13 21:06:22 +0200

    2

---
+++ src/components/Profile.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src='https://www.electrive.com/wp-content/uploads/2018/06/byd-adl-enviro400ev-elektrobus-electric-bus-london.png' />
+            </div>
+            <div>Ava + description</div>
+            <div>My post</div>
+            <div>Post1</div>
+            <div>Post2</div>
+        </div>
+    );
+}
+// tect
+export default Profile;
