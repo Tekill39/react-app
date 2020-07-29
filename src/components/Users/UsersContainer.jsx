99a61ccd036688bd14046b24aa67c861c2b463e7
Tekill39 <minit009@yandex.ru>
2020-07-29 20:14:54 +0200

    29_07

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,7 +1,35 @@
-
+import React from 'react';
 import { connect } from 'react-redux';
-import Users from './Users';
-import { followAC, unfollowAC, setUsersAC, setcurrentPageAC } from '../../redux/users-reducer';
+import { followAC, unfollowAC, setUsersAC, setcurrentPageAC,setUsersTotalCountAC } from '../../redux/users-reducer';
+import Users from "./Users";
+import * as axios from 'axios';
+
+class UsersContainer extends React.Component {
+
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+            this.props.setUsers(response.data.items);
+            this.props.setTotalUsersCount(response.data.totalCount);
+        });
+    }
+    onPageChanged = (pageNumber)=>{
+        this.props.setCurrentPage(pageNumber)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
+            this.props.setUsers(response.data.items);
+        });
+    }
+
+    render() {
+
+        return <Users totalUsersCount = {this.props.totalUsersCount} 
+        pageSize = {this.props.pageSize}
+        currentPage = {this.props.currentPage}
+        onPageChanged = {this.onPageChanged}
+        users ={this.props.users}
+        follow ={this.props.follow}
+        unfollow ={this.props.unfollow}/>
+    }
+}
 
 
 let mapStateToProps=(state) => {
@@ -25,8 +53,11 @@ let mapDispatchToProps=(dispatch)=>{
         },
         setCurrentPage:(pageNumber)=>{
             dispatch(setcurrentPageAC(pageNumber))
+        },
+        setTotalUsersCount:(totalCount)=>{
+            dispatch(setUsersTotalCountAC(totalCount))
         }
     }
 }
 
-export default connect(mapStateToProps,mapDispatchToProps)(Users);
+export default connect(mapStateToProps,mapDispatchToProps)(UsersContainer);
