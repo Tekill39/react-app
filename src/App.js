9ec20e7bf00cf05c9497f62473405b2a861c25f7
Tekill39 <minit009@yandex.ru>
2020-12-04 10:17:25 +0200

    source

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import React from 'react';
 import './App.css';
 import HeaderContainer from './components/Header/HeaderContainer';
 import Navbar from './components/Navbar/Navbar';
-import ProfileContainer from './components/Profile/ProfileContainer';
 import {Route} from "react-router-dom";
-import DialogsContainer from './components/Dialogs/DialogsContainer';
 import UsersContainer from './components/Users/UsersContainer';
 import LoginPage from './components/login/login';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
 import {withRouter} from 'react-router-dom';
 import {initialaizeApp} from './redux/app-reducer';
+import {withSuspense} from './hoc/withSuspense';
+
+
+// import DialogsContainer from './components/Dialogs/DialogsContainer';
+// import ProfileContainer from './components/Profile/ProfileContainer';
+const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
+const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'));
 
 class App extends React.Component {  
   componentDidMount() {
@@ -22,8 +27,8 @@ class App extends React.Component {
       <HeaderContainer/>
       <Navbar />
       <div className='app-wrapper-content'>
-         <Route path='/profile/:userId?' render = { ()=> <ProfileContainer /> } /> 
-         <Route path='/dialogs' render = { ()=> <DialogsContainer/>} />
+         <Route path='/profile/:userId?' render = {withSuspense(ProfileContainer)} /> 
+         <Route path='/dialogs' render = { withSuspense(DialogsContainer)} />
          <Route path='/users' render = { ()=> <UsersContainer/>} />      
          <Route path='/login' render = { ()=> <LoginPage/>} />                  
       </div>
--- src/components/Users/Paginator.jsx
+++ src/components/Users/Paginator.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import styles from './Paginator.module.css';
+import cn from "classnames";
 
 let Paginator = ({totalItemsCount,pageSize,currentPage,onPageChanged,portisionSize=20}) => {
 
@@ -20,7 +21,7 @@ let Paginator = ({totalItemsCount,pageSize,currentPage,onPageChanged,portisionSi
         {pages
         .filter(p=> p >= leftPortionPageNumber && p<=rightPortionPageNumber)
         .map((p)=> {
-            return <span className={({
+            return <span className={cn({
                 [styles.selectedPage] : currentPage === p},
                 styles.pageNumber) }
                 key={p}
